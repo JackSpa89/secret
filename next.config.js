0ea293a44c3db2d4feedbf1f51ddc0182c66cfc0
JackSpa89 <48829281+jackspa89@users.noreply.github.com>
2023-01-02 13:05:25 +0100

    Create nextjs.yml

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     publicRuntimeConfig: {
+        productionBrowserSourceMaps: true,
         firebaseApiKey: process.env.FIREBASE_API_KEY,
         firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
         firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
