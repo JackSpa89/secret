8b7b6a7871ead3ff7e6d2367d667ca6198d05d4d
p.mumber <p.mumber@web.de>
2023-01-06 05:59:21 +0100

    adding env variables to yml

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     productionBrowserSourceMaps: true,
     publicRuntimeConfig: {
-        productionBrowserSourceMaps: true,
         firebaseApiKey: process.env.FIREBASE_API_KEY,
         firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
         firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
