5dfd70263110da505037227c9fb4f2230f79f506
p.mumber <p.mumber@web.de>
2023-01-06 18:31:30 +0100

    Version 1.1

--- src/components/pages/Rsvp/Rsvp.tsx
+++ src/components/pages/Rsvp/Rsvp.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Link, Typography } from '@mui/material';
 import Head from 'next/head';
 
 import { AppMenu } from '~components/organism/AppMenu/AppMenu';
@@ -16,8 +17,16 @@ export const RsvpPage: React.FC = () => {
             </Head>
             <AppMenu />
             <div className={ style.rsvp__wrapper }>
-                <div className={ style.rsvp__intro }>Please RSVP by January 31st 2023!</div>
+                <div className={ style.rsvp__intro }>Please RSVP by February 15th, 2023!</div>
                 { isMobile() && <img alt="be our guest mobile" src={ './beOurGuestMobile.jpeg' } className={ style.rsvp__image } />}
+                <div className={ style.rsvp__venue }>Location:</div>
+                <div className={ style.rsvp__venue }>Buhl Mansion Guesthouse & Spa</div>
+                <div className={ style.rsvp__venue }>422 E. State Street</div>
+                <div className={ style.rsvp__venue }>Sharon, PA 16146</div>
+                <Typography align='center'>
+                    <Link href="https://www.buhlmansion.com/" className={ style.rsvp__venue }>Link to the Buhl Mansion Website</Link>
+                </Typography>
+                <div className={ style.rsvp__venue }>The ceremony will start promptly at 4pm.</div>
                 <SignupForm />
             </div>
         </div>
